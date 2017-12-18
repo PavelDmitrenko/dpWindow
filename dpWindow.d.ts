26d1b1cb808ac9f345fbc9e899e53681a951475c
PavelDmitrenko <dmitrenko@fas.gov.ru>
2017-12-18 12:54:15 +0300

    Init

---
+++ dpWindow.d.ts
@@ -0,0 +1,86 @@
+﻿// Type definitions for [~THE LIBRARY NAME~] [~OPTIONAL VERSION NUMBER~]
+// Project: dpWindow
+// Definitions by: Pavel Dmitrenko <https://github.com/PavelDmitrenko/dpWindow>
+
+
+
+//declare class dpWindow {
+//	Show(options: any): void;
+//}
+
+
+interface JQueryStatic {
+	dpModal(options?: any);
+}
+
+interface IDPWOptions {
+
+	id: string;
+
+	content: IDPWContentSettings;
+	size: IDPWClientSize;
+	appearence?: IDPWAppearence;
+	closeSelectors?: string; // Selectors which actions as current window closers (ex: button with "Close" meaning)
+
+	struct?: IDPWStructSettings;
+
+	closeOnOuterMouseClick?: boolean; // Close window on mouse click outside window bounds
+
+	log?: boolean;
+
+	// Events
+	onLoaded?(wnd: any): void;
+	//onBeforeClose?(deffered: JQueryDeferred<boolean>, sender: JQuery): boolean;
+	onBeforeClose?(deffered: any, sender: JQuery): void;
+	onContentPrefilter?(response: Object): string;
+	onClose?(): void;
+}
+
+interface IDPWHeader {
+	setCaption(text: string);
+}
+
+interface IDPWContentSettings {
+	url?: string;
+	urlPostMethod? :string;
+	urlPostData?: any;
+	html?: string;
+}
+
+interface IDPWStructSettings {
+	header?: IDPWHeaderSettings;
+	footer?: IDPWFooterSettings;
+	createMain: boolean;
+}
+
+interface IDPWHeaderSettings {
+	visible?: boolean;
+	content?: string;
+	showCloseButton?: boolean;
+}
+
+interface IDPWFooterSettings {
+	visible?: boolean;
+	content?: string;
+}
+
+interface IDPWClientSize {
+	width: any;
+	height: any;
+	zindex?: number;
+}
+
+interface IDPWSize {
+	width?: number;
+	height?: number;
+	left?: number;
+	top?: number;
+	zindex?: number;
+}
+
+interface IDPWAppearence {
+	color?: string;
+	bgColor?: string;
+	bgOpacity?: number;
+	className?: string;
+}
